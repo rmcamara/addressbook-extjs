1d94d22d0cabdb0852288c8c69ce2901a0b45ac9
Ross Camara <rmcamara@users.noreply.github.com>
2012-06-27 00:05:47 -0400

    Added coffeescript build files

---
+++ build/coffeescript directory watcher.bat
@@ -0,0 +1,10 @@
+@ECHO OFF
+COLOR F0
+ECHO .
+ECHO To use CoffeeScript, first install Node.js from nodejs.org
+ECHO Next, download the CoffeeScript module by opening a command prompt and typing: npm install -g coffee-script
+ECHO That's it.
+ECHO .
+
+call cake watch
+pause
